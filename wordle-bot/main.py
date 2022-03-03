3042802a63d6059233fb61f39ee21504ba041507
ralphtuci <33015813+ralphtuci@users.noreply.github.com>
2022-03-03 07:49:43 -0800

    Update main.py
    less lines

--- wordle-bot/main.py
+++ wordle-bot/main.py
@@ -21,7 +21,7 @@ replit.clear()
 # main code
 
 # retrieving word
-WORDS = word.makelist()
+words = word.makelist()
 
 # sending to discord client
 client = discord.Client()
@@ -29,29 +29,23 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
 @client.event
 async def on_message(message):
-  
-  
     if message.author == client.user:
         return
 
     if message.content.startswith('$easy'):
-        CHOICE = word.getword(1, WORDS)
-        await message.channel.send("||" + CHOICE + "||")
+        await message.channel.send("||" + word.geteasy(words) + "||")
 
     if message.content.startswith('$word'):
-        CHOICE = word.getword(2, WORDS)
-        await message.channel.send("||" + CHOICE + "||")
+        await message.channel.send("||" + word.getword(words) + "||")
 
     if message.content.startswith('$hard'):
-        CHOICE = word.getword(3, WORDS)
-        await message.channel.send("||" + CHOICE + "||")
+        await message.channel.send("||" + word.gethard(words) + "||")
 
     if message.content.startswith('$ex'):
-        CHOICE = word.getword(4, WORDS)
-        await message.channel.send("||" + CHOICE + "||")
+        await message.channel.send("||" + word.getex(words) +   "||")
+
 
 client.run(os.getenv('TOKEN'))
 
